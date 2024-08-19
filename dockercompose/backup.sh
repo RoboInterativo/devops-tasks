c5800637e762a191ca977706d434aef6e39a1cdf
chiefexb <chief_exb@mail.ru>
2024-08-20 02:27:20 +0300

    123

--- dockercompose/backup.sh
+++ dockercompose/backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+TARGET= /opt/docker/dockercompose/task-13
 if [ ! -d directory ]; then
-  mkdir /opt/docker/dockercompose/task-13/
+  mkdir $TARGET
 fi
 if [  -d directory ]; then
   echo /opt/docker/dockercompose/task-13/ exist nothing to do
 fi
 docker run --network dockercompose-frontend \
---volume /opt/docker/dockercompose/task-13:/backup \
---rm mariadb:lts mariadb-backup --help
+--volume $TARGET:/backup \
+--rm mariadb:lts mariadb-backup --backup --target-dir=/backup
+--databases=mydb --user root --host=mydb --password=rootpassword
