57dda50ad05c748c4ac1126cf07d3a6190c1ebc3
chiefexb <chief_exb@mail.ru>
2024-08-21 23:59:50 +0300

    123

--- storage/task4.3.sh
+++ storage/task4.3.sh
@@ -1,1 +1,5 @@
-docker exec -it tmpfs_container "dd if=/dev/zero of=/my_folder1/speed_file >/my_folder1/result"
+docker exec -it tmpfs_container\
+
+dd if=/dev/zero of=/my_folder1/speed_file bs=1G count=1 >/my_folder1/result
+
+echo Hello from my_file1! >/my_folder1/my_file1
