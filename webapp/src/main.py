7bff4efa9c3ad5ca3577e2c9dad727bd5844c0ca
chiefexb <chief_exb@mail.ru>
2025-06-21 18:05:22 +0300

    123

--- webapp/src/main.py
+++ webapp/src/main.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-# Конфигурация базы данных
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/db_name'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 # Модель данных
 class User(db.Model):
@@ -18,32 +12,40 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-# Создание таблиц (выполнить один раз)
-@app.before_first_request
-def create_tables():
-    db.create_all()
+def create_app():
+    app = Flask(__name__)
 
-# Маршруты
-@app.route('/')
-def index():
-    users = User.query.all()
-    return render_template('index.html', users=users)
+    # Конфигурация базы данных
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/db_name'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/add', methods=['POST'])
-def add_user():
-    username = request.form['username']
-    email = request.form['email']
-    new_user = User(username=username, email=email)
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect(url_for('index'))
+    db = SQLAlchemy(app)
+    # Создание таблиц (выполнить один раз)
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
-@app.route('/delete/<int:id>')
-def delete_user(id):
-    user = User.query.get_or_404(id)
-    db.session.delete(user)
-    db.session.commit()
-    return redirect(url_for('index'))
+    # Маршруты
+    @app.route('/')
+    def index():
+        users = User.query.all()
+        return render_template('index.html', users=users)
 
+    @app.route('/add', methods=['POST'])
+    def add_user():
+        username = request.form['username']
+        email = request.form['email']
+        new_user = User(username=username, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    @app.route('/delete/<int:id>')
+    def delete_user(id):
+        user = User.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+app = create_app()
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5555)
