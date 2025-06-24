837f05cbf4ca8139bb676ca62b52d00f0e9aa086
a.shilo <a.shilo@baum.ru>
2025-06-24 12:09:03 +0300

    123

--- pythonbot/src/settings.py
+++ pythonbot/src/settings.py
@@ -1,2 +1,6 @@
-admin_ids = ["TELEGRAM ID OF ADMIN"]
+admin_ids = ["323687805","1094307608"]
 TOKEN = '6451090037:AAGJF_VCV_qeedbK4vLWE3nONqwLd-Jc8a8'
+
+# Your user ID: 323687805
+# Current chat ID: 323687805
+# Forwarded from: 1094307608
